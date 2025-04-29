cd454ef4e7fa163a092bdb7af4d97cc5d1e83a11
Avinash Anish <143164352+CubeStar1@users.noreply.github.com>
2025-04-29 08:27:44 +0530

    Update team page.tsx

--- app/(marketing)/team/page.tsx
+++ app/(marketing)/team/page.tsx
@@ -1,4 +1,4 @@
-export default function () {
+export default function TeamPage() {
     return (<div className="flex justify-center items-center h-screen">
     <h1 className="text-yellow-200">Hello world</h1>
   </div>);
