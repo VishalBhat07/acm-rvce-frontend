be52c0b6733fe8f15aaf55d502a56b0e6bcc1137
Tharun M <tharun151425@gmail.com>
2025-05-09 06:59:02 +0530

    Added Team Images

--- lib/config/teamData.ts
+++ lib/config/teamData.ts
@@ -20,34 +20,34 @@ export const teamData: TeamData = {
   // },
   "2024": {
     core: [
-      { id: 1, name: "Tanisha Agarwal", role: "Chair", image: "team\\team4.jpg", email: "Tanisha@acm.org", linkedin: "https://www.linkedin.com/in/tanishaagarwal003/", github: "https://github.com" },
-      { id: 2, name: "Mehar Kulkarni", role: "Vice Chair", image: "team\\team2.jpg", email: "Mehar@acm.org", linkedin: "https://www.linkedin.com/in/meharkulkarni/", github: "https://github.com" },
-      { id: 3, name: "Ayush Ojha", role: "Treasurer", image: "team\\team6.jpg", email: "Ayush@acm.org", linkedin: "https://www.linkedin.com/in/ojhaayush2003/", github: "https://github.com" },
-      { id: 4, name: "Prakhar Jain", role: "Secretary", image: "team\\team7.webp", email: "{Prakhar@acm.org", linkedin: "https://www.linkedin.com/in/prakhar-jain-ab2a20166/", github: "https://github.com" },
-      { id: 5, name: "Aryan Chaturvedi", role: "Membership Head", image: "team\\team5.jpg", email: "Aryan@acm.org", linkedin: "https://www.linkedin.com/in/aryan-chaturvedi-2002m/", github: "https://github.com" },
+      { id: 1, name: "Tanisha Agarwal", role: "Chair", image: "team\\Actual_CT\\Senior_CT\\Tanisha_A.jpg", email: "Tanisha@acm.org", linkedin: "https://www.linkedin.com/in/tanishaagarwal003/", github: "https://github.com" },
+      { id: 2, name: "Mehar Kulkarni", role: "Vice Chair", image: "team\\Actual_CT\\Senior_CT\\Mehar_K_1.jpg", email: "Mehar@acm.org", linkedin: "https://www.linkedin.com/in/meharkulkarni/", github: "https://github.com" },
+      { id: 3, name: "Ayush Ojha", role: "Treasurer", image: "team\\Actual_CT\\Senior_CT\\Ayush_O_1.jpg", email: "Ayush@acm.org", linkedin: "https://www.linkedin.com/in/ojhaayush2003/", github: "https://github.com" },
+      { id: 4, name: "Prakhar Jain", role: "Secretary", image: "team\\Actual_CT\\Senior_CT\\Prakhar_J_1.jpg", email: "{Prakhar@acm.org", linkedin: "https://www.linkedin.com/in/prakhar-jain-ab2a20166/", github: "https://github.com" },
+      { id: 5, name: "Aryan Chaturvedi", role: "Membership Head", image: "team\\Actual_CT\\Senior_CT\\Aryan_C_1.jpg", email: "Aryan@acm.org", linkedin: "https://www.linkedin.com/in/aryan-chaturvedi-2002m/", github: "https://github.com" },
     ],
     junior: [
-      { id: 6, name: "Billy Butcher", role: "Event Coordinator", image: "team\\junior1.jpg", email: "casey@acm.org", linkedin: "https://linkedin.com", github: "https://github.com" },
-      { id: 7, name: "M.M.", role: "Technical Lead", image: "team\\junior2.jpg", email: "jordan@acm.org", linkedin: "https://linkedin.com", github: "https://github.com" },
-      { id: 8, name: "Hughie Campbell", role: "Marketing Lead", image: "team\\junior3.jpg", email: "riley@acm.org", linkedin: "https://linkedin.com", github: "https://github.com" },
-      { id: 9, name: "Frenchie", role: "Design Lead", image: "team\\junior4.jpg", email: "quinn@acm.org", linkedin: "https://linkedin.com", github: "https://github.com" },
-      { id: 10, name: "Kimiko Miyashiro", role: "Social Lead", image: "team\\junior5.jpg", email: "quinn@acm.org", linkedin: "https://linkedin.com", github: "https://github.com" },
+      { id: 6, name: "Anirudh Kulkarni", role: "Junior Core", image: "team\\Filler\\junior1.jpg", email: "casey@acm.org", linkedin: "https://linkedin.com", github: "https://github.com" },
+      { id: 7, name: "Yash Sarogi", role: "Junior Core", image: "team\\Filler\\junior2.jpg", email: "jordan@acm.org", linkedin: "https://linkedin.com", github: "https://github.com" },
+      { id: 8, name: "Anirudh Sai", role: "Junior Core", image: "team\\Filler\\junior3.jpg", email: "riley@acm.org", linkedin: "https://linkedin.com", github: "https://github.com" },
+      { id: 9, name: "Smruthi S K", role: "Junior Core", image: "team\\Filler\\junior5.jpg", email: "quinn@acm.org", linkedin: "https://linkedin.com", github: "https://github.com" },
+      { id: 10, name: "Aryan Rai", role: "Junior Core", image: "team\\Filler\\junior4.jpg", email: "quinn@acm.org", linkedin: "https://linkedin.com", github: "https://github.com" },
     ]
   },
   "2025": {
     core: [
-      { id: 1, name: "Tanisha Agarwal", role: "Chair", image: "team\\team4.jpg", email: "Tanisha@acm.org", linkedin: "https://www.linkedin.com/in/tanishaagarwal003/", github: "https://github.com" },
-      { id: 2, name: "Mehar Kulkarni", role: "Vice Chair", image: "team\\team2.jpg", email: "Mehar@acm.org", linkedin: "https://www.linkedin.com/in/meharkulkarni/", github: "https://github.com" },
-      { id: 3, name: "Ayush Ojha", role: "Treasurer", image: "team\\team6.jpg", email: "Ayush@acm.org", linkedin: "https://www.linkedin.com/in/ojhaayush2003/", github: "https://github.com" },
-      { id: 4, name: "Prakhar Jain", role: "Secretary", image: "team\\team7.webp", email: "Prakhar@acm.org", linkedin: "https://www.linkedin.com/in/prakhar-jain-ab2a20166/", github: "https://github.com" },
-      { id: 5, name: "Aryan Chaturvedi", role: "Membership Head", image: "team\\team5.jpg", email: "Aryan@acm.org", linkedin: "https://www.linkedin.com/in/aryan-chaturvedi-2002m/", github: "https://github.com" },
+      { id: 1, name: "Tanisha Agarwal", role: "Chair", image: "team\\Actual_CT\\Senior_CT\\Tanisha_A.jpg", email: "Tanisha@acm.org", linkedin: "https://www.linkedin.com/in/tanishaagarwal003/", github: "https://github.com" },
+      { id: 2, name: "Mehar Kulkarni", role: "Vice Chair", image: "team\\Actual_CT\\Senior_CT\\Mehar_K_1.jpg", email: "Mehar@acm.org", linkedin: "https://www.linkedin.com/in/meharkulkarni/", github: "https://github.com" },
+      { id: 3, name: "Ayush Ojha", role: "Treasurer", image: "team\\Actual_CT\\Senior_CT\\Ayush_O_1.jpg", email: "Ayush@acm.org", linkedin: "https://www.linkedin.com/in/ojhaayush2003/", github: "https://github.com" },
+      { id: 4, name: "Prakhar Jain", role: "Secretary", image: "team\\Actual_CT\\Senior_CT\\Prakhar_J_1.jpg", email: "Prakhar@acm.org", linkedin: "https://www.linkedin.com/in/prakhar-jain-ab2a20166/", github: "https://github.com" },
+      { id: 5, name: "Aryan Chaturvedi", role: "Membership Head", image: "team\\Actual_CT\\Senior_CT\\Aryan_C_1.jpg", email: "Aryan@acm.org", linkedin: "https://www.linkedin.com/in/aryan-chaturvedi-2002m/", github: "https://github.com" },
     ],
     junior: [
-      { id: 6, name: "Billy Butcher", role: "Event Coordinator", image: "team\\junior1.jpg", email: "casey@acm.org", linkedin: "https://linkedin.com", github: "https://github.com" },
-      { id: 7, name: "M.M.", role: "Technical Lead", image: "team\\junior2.jpg", email: "jordan@acm.org", linkedin: "https://linkedin.com", github: "https://github.com" },
-      { id: 8, name: "Hughie Campbell", role: "Marketing Lead", image: "team\\junior3.jpg", email: "riley@acm.org", linkedin: "https://linkedin.com", github: "https://github.com" },
-      { id: 9, name: "Frenchie", role: "Design Lead", image: "team\\junior4.jpg", email: "quinn@acm.org", linkedin: "https://linkedin.com", github: "https://github.com" },
-      { id: 10, name: "Kimiko Miyashiro", role: "Social Lead", image: "team\\junior5.jpg", email: "quinn@acm.org", linkedin: "https://linkedin.com", github: "https://github.com" },
+      { id: 6, name: "Anirudh Kulkarni", role: "Junior Core", image: "team\\Filler\\junior1.jpg", email: "casey@acm.org", linkedin: "https://linkedin.com", github: "https://github.com" },
+      { id: 7, name: "Yash Sarogi", role: "Junior Core", image: "team\\Filler\\junior2.jpg", email: "jordan@acm.org", linkedin: "https://linkedin.com", github: "https://github.com" },
+      { id: 8, name: "Anirudh Sai", role: "Junior Core", image: "team\\Filler\\junior3.jpg", email: "riley@acm.org", linkedin: "https://linkedin.com", github: "https://github.com" },
+      { id: 9, name: "Smruthi S K", role: "Junior Core", image: "team\\Filler\\junior5.jpg", email: "quinn@acm.org", linkedin: "https://linkedin.com", github: "https://github.com" },
+      { id: 10, name: "Aryan Rai", role: "Junior Core", image: "team\\Filler\\junior4.jpg", email: "quinn@acm.org", linkedin: "https://linkedin.com", github: "https://github.com" },
     ]
   }
 }; 
