dc16aa5601f37e60d98967c0d6c15c7e11ebbdde
CubeStar1 <143164352+CubeStar1@users.noreply.github.com>
2025-06-15 10:35:23 +0530

    Chore: Fixed build error

--- components/about/acm-section.tsx
+++ components/about/acm-section.tsx
@@ -1,6 +1,6 @@
 "use client";
 
-import { motion } from "framer-motion";
+import { motion, Variants } from "framer-motion";
 import Image from "next/image";
 import { acmSectionConfig } from "@/lib/config/about-page-config";
 
@@ -15,7 +15,7 @@ const container = {
   },
 };
 
-const item = {
+const item: Variants = {
   hidden: { opacity: 0, y: 20 },
   show: {
     opacity: 1,
