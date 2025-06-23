e61991e27127c76253e7d76f0b37c64b6eab4a3d
CubeStar1 <143164352+CubeStar1@users.noreply.github.com>
2025-06-23 09:55:24 +0530

    Chore: Updated revalidate

--- app/(marketing)/blog/page.tsx
+++ app/(marketing)/blog/page.tsx
@@ -2,8 +2,8 @@ import BlogPageClient from "@/components/blog/blog-page-client";
 import { sanityFetch } from "@/sanity/lib/fetch";
 import { postsQuery } from "@/sanity/lib/queries";
 
-// Revalidate all posts every hour
-export const revalidate = 3600
+// Revalidate all posts every 10 minutes
+export const revalidate = 600
 
 export default async function BlogPage() {
   const posts = await sanityFetch({
